b6da1d91bd580cef19f61e63ff331dd072340297
Satish Kumar <skumar5@onetrust.com>
2021-08-17 08:08:31 -0400

    twosum

--- easyproblems/src/TestApplication.java
+++ easyproblems/src/TestApplication.java
@@ -1,7 +1,13 @@
+import java.util.Arrays;
+
 public class TestApplication {
     public static void main(String[] args) {
-        MaxConsecutiveOnes m1= new MaxConsecutiveOnes();
-        int[] num = new int[]{1,1,0,1,1,1};
+        MaxConsecutiveOnes m1 = new MaxConsecutiveOnes();
+        int[] num = new int[]{1, 1, 0, 1, 1, 1};
         System.out.println(m1.findMaxConsecutiveOnes(num));
+
+
+        TwoSum t= new TwoSum();
+        System.out.println(Arrays.toString(t.findSum(new int[]{2,7,11,15}, 9)));
     }
 }
---
+++ easyproblems/src/TwoSum.java
@@ -0,0 +1,48 @@
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+
+
+Constraints:
+
+2 <= nums.length <= 104
+-109 <= nums[i] <= 109
+-109 <= target <= 109
+Only one valid answer exists.
+
+ */
+
+import java.util.HashMap;
+
+public class TwoSum {
+    public int[] findSum(int nums[], int target){
+        HashMap<Integer, Integer> h = new HashMap<>();
+        for(int i = 0; i<nums.length; i++){
+            int compliment =  target - nums[i];
+            if(h.containsKey(compliment)){
+                return new int[]{i, h.get(compliment)};
+            }
+            h.put(nums[i], i);
+        }
+        return null;
+    }
+}
